fdb48e41e3924e23b7bb419fe1b240388f6180cd
dcdholder <dcdholder@gmail.com>
2017-01-30 10:41:57 -0500

    Added Inventory test skeleton.

---
+++ test/com/chilked/ripscape/TestInventory.java
@@ -0,0 +1,56 @@
+package com.chilked.ripscape;
+
+import org.junit.Test;
+
+public class TestInventory {
+	
+	@Test
+	void testInventoryAdd() {
+		
+	}
+	
+	@Test
+	void testInventoryAddWhenFull() {
+		
+	}
+	
+	@Test
+	void testInventoryViewFullIndex() {
+		
+	}
+	
+	@Test
+	void testInventoryViewEmptyIndex() {
+		
+	}
+	
+	@Test
+	void testInventoryInvalidIndex() {
+		
+	}
+	
+	@Test
+	void testInventoryRetrieveFullIndex() {
+		
+	}
+	
+	@Test
+	void testInventoryRetrieveEmptyIndex() {
+		
+	}
+	
+	@Test
+	void testInventoryRetrieveInvalidIndex() {
+		
+	}
+	
+	@Test
+	void testInventoryRetrieveIndicesObjectExists() {
+		
+	}
+	
+	@Test
+	void testInventoryRetrieveIndicesObjectDoesNotExist() {
+		
+	}
+}
